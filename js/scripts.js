134346b60390e5559bcf499963092cf7c74a7a2f
Mike Manchee <mikemanchee@gmail.com>
2020-08-12 13:51:57 -0700

    add jquery to interface with html
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>
    Co-authored-by: Drew Macauley <macauleyandy03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,9 @@ funtction divide(number1, number2) {
   return number1 / number2;
 };
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-const result = add(number1, number2);
-
-alert(add(number1, number2));
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+  
+  alert(add(number1, number2));
+});
