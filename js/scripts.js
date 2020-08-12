973f3a4771ade3d27ca7113a4ab527ef9b97a515
Mike Manchee <mikemanchee@gmail.com>
2020-08-12 13:46:15 -0700

    add html form and js
    Co-authored-by: Drew Parker <drewjparker91@gmail.com>
    Co-authored-by: Drew Macauley <macauleyandy03@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 function add(number1, number2) {
   return number1 + number2;
-}
+};
 
 funtction subtract(number1, number2) {
   return number1 - number2;
-}
+};
 
 funtction multiply(number1, number2) {
   return number1 * number2;
-}
+};
 
 funtction divide(number1, number2) {
   return number1 / number2;
-}
+};
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-const result = add(number1, number2)
+const result = add(number1, number2);
 
 alert(add(number1, number2));
